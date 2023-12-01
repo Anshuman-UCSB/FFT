db02f7e2d3ed875c5484199ca1e8118c5694d3fa
Anshuman Dash <anshuman@ucsb.edu>
2023-11-30 20:52:12 -0800

    issue with javaCV installation

--- FFT_video/app/src/main/java/com/example/fft_video/FrameExtractor.java
+++ FFT_video/app/src/main/java/com/example/fft_video/FrameExtractor.java
@@ -8,31 +8,16 @@ import android.util.Log;
 import java.util.ArrayList;
 import java.util.List;
 
-import wseemann.media.FFmpegMediaMetadataRetriever;
 
 public class FrameExtractor {
     private static final String TAG = "FrameExtractor";
-    private final FFmpegMediaMetadataRetriever retriever;
     private Context context;
     public FrameExtractor(Context context){
         this.context = context;
-        retriever = new FFmpegMediaMetadataRetriever();
     }
     public List<Bitmap> extractFrames(Uri videoUri) {
         List<Bitmap> frames = new ArrayList<>();
 
-        retriever.setDataSource(context, videoUri);
-        String duration = retriever.extractMetadata(FFmpegMediaMetadataRetriever.METADATA_KEY_DURATION);
-        long videoDuration = Long.parseLong(duration) * 1000; // in microseconds
-        Log.i(TAG, "Duration is "+videoDuration+"us");
-        Log.i(TAG, "KEY FRAMERATE is "+retriever.extractMetadata(FFmpegMediaMetadataRetriever.METADATA_KEY_FRAMERATE));
-
-        for (long time = 0; time < videoDuration; time += 100000) { // Extract frame every second
-            Bitmap frame = retriever.getFrameAtTime(time, FFmpegMediaMetadataRetriever.OPTION_CLOSEST_SYNC);
-            frames.add(frame);
-        }
-
-        retriever.release();
         return frames;
     }
 }
