9621b1715d37a509a7a27071fa535e7b27caf18c
Anshuman Dash <anshuman@ucsb.edu>
2023-12-07 19:46:05 -0800

    adding graphic overlay

--- pose_video/app/src/main/java/com/fft/pose_video/MainActivity.java
+++ pose_video/app/src/main/java/com/fft/pose_video/MainActivity.java
@@ -1,14 +1,132 @@
 package com.fft.pose_video;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.PickVisualMediaRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.SurfaceTexture;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Size;
+import android.view.Surface;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.FrameLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.ui.PlayerView;
+
+public class MainActivity extends AppCompatActivity implements TextureView.SurfaceTextureListener {
+    private static final String TAG = "FFT_main";
+    private SimpleExoPlayer player;
+    private PlayerView playerView;
+
+    private TextureView textureView;
+    private ActivityResultLauncher<PickVisualMediaRequest> pickMedia;
+    private Surface playerSurface;
+    private SurfaceTexture surfaceTexture;
+    private GraphicOverlay graphicOverlay;
+
+    private CustomPoseDetector imageProcessor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageProcessor = new CustomPoseDetector();
+
+        player = new SimpleExoPlayer.Builder(this).build();
+
+        playerView = findViewById(R.id.player_view);
+        playerView.setPlayer(player);
+        FrameLayout contentFrame = playerView.findViewById(com.google.android.exoplayer2.ui.R.id.exo_content_frame);
+        View videoFrameView = createVideoFrameView();
+        if(videoFrameView != null) contentFrame.addView(videoFrameView);
+
+        graphicOverlay = new GraphicOverlay(this, null);
+        contentFrame.addView(graphicOverlay);
+
+        pickMedia = registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), uri -> {
+            if (uri != null) {
+                Log.d(TAG, "Selected URI: " + uri);
+                setupPlayer(uri);
+            } else {
+                Log.d(TAG,"No media selected");
+            }
+        });
+
+        findViewById(R.id.selectVidBtn).setOnClickListener( v -> {
+            startSelectVideo();
+        });
+    }
+
+    private View createVideoFrameView() {
+        textureView = new TextureView(this);
+        textureView.setSurfaceTextureListener(this);
+        return textureView;
+    }
+
+    private void setupPlayer(Uri uri){
+        MediaItem mediaItem = MediaItem.fromUri(uri);
+        player.stop();
+        player.setMediaItem(mediaItem);
+        player.prepare();
+        player.play();
+    }
+
+    private void startSelectVideo() {
+        pickMedia.launch(new PickVisualMediaRequest.Builder()
+                .setMediaType(ActivityResultContracts.PickVisualMedia.VideoOnly.INSTANCE)
+                .build());
+    }
+
+    @Override
+    public void onSurfaceTextureAvailable(@NonNull SurfaceTexture surface, int width, int height) {
+        surfaceTexture = surface;
+        playerSurface = new Surface(surface);
+        player.setVideoSurface(playerSurface);
+    }
+
+    @Override
+    public void onSurfaceTextureSizeChanged(@NonNull SurfaceTexture surface, int width, int height) {
+
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(@NonNull SurfaceTexture surface) {
+        player.setVideoSurface(null);
+        playerSurface.release();
+        playerSurface = null;
+        surfaceTexture.release();
+        surfaceTexture = null;
+        return true;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(@NonNull SurfaceTexture surface) {
+        Size size = getSizeForDesiredSize(textureView.getWidth(), textureView.getHeight(), 500);
+        processFrame(textureView.getBitmap(size.getWidth(), size.getHeight()));
+    }
+
+    private void processFrame(Bitmap bitmap) {
+        imageProcessor.getPose(bitmap);
+    }
+
+    private Size getSizeForDesiredSize(int width, int height, int desiredSize){
+        int w, h;
+        if(width > height){
+            w = desiredSize;
+            h = Math.round((height/(float)width) * w);
+        }else{
+            h = desiredSize;
+            w = Math.round((width/(float)height) * h);
+        }
+        return new Size(w, h);
     }
 }
