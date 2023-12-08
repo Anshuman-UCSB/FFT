614d3e32d526c1eb6e464505e22ac1df487f8d85
Anshuman Dash <anshuman@ucsb.edu>
2023-12-07 23:42:31 -0800

    Login with email working

--- FFT/app/src/main/java/com/fft/fft/MainActivity.java
+++ FFT/app/src/main/java/com/fft/fft/MainActivity.java
@@ -2,13 +2,43 @@ package com.fft.fft;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
+    Button button;
+    TextView greeter;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+        button = findViewById(R.id.logout);
+        greeter = findViewById(R.id.greeter);
+        user = auth.getCurrentUser();
+        if (user == null){
+            goToLogin();
+        }
+        greeter.setText(user.getEmail());
+
+        button.setOnClickListener(v->{
+            auth.signOut();
+            goToLogin();
+        });
+    }
+    private void goToLogin(){
+        Intent intent = new Intent(getApplication(), Login.class);
+        startActivity(intent);
+        finish();
     }
 }
